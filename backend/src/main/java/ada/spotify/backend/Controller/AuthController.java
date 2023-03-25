9d9c43307d0465cf0ee62c92ad95e566ed262f6d
Renata Inacio <revazgauska@gmail.com>
2023-03-25 01:31:18 -0300

    autenticacao v2

--- backend/src/main/java/ada/spotify/backend/Controller/AuthController.java
+++ backend/src/main/java/ada/spotify/backend/Controller/AuthController.java
@@ -2,21 +2,29 @@ package ada.spotify.backend.Controller;
 
 import ada.spotify.backend.APIs.Keys;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.SpotifyHttpManager;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import se.michaelthelin.spotify.model_objects.special.SearchResult;
 import se.michaelthelin.spotify.model_objects.specification.Artist;
 import se.michaelthelin.spotify.model_objects.specification.Paging;
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 import se.michaelthelin.spotify.requests.data.personalization.simplified.GetUsersTopArtistsRequest;
+import se.michaelthelin.spotify.requests.data.search.SearchItemRequest;
+import se.michaelthelin.spotify.requests.data.search.simplified.SearchArtistsRequest;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpResponse;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -34,18 +42,18 @@ public class AuthController {
 
     @GetMapping("login")
     @ResponseBody
-    public String spotifyLogin() {
+    public ResponseEntity<Void> spotifyLogin() {
         AuthorizationCodeUriRequest autorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
                 .scope("user-read-private, user-read-email, user-top-read")
                 .show_dialog(true)
                 .build();
         final URI uri = autorizationCodeUriRequest.execute();
-        return uri.toString();
+        return ResponseEntity.status(HttpStatus.FOUND).location(uri).build();
     }
 
-    @GetMapping(value="get-user-code")
-    public String getSpotifyUserCode(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
-//        code = userCode;
+    @GetMapping(value="get-user-code/")
+    public String getSpotifyUserCode(@RequestParam("code") String userCode, HttpServletResponse response) throws IOException {
+        code = userCode;
         AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code)
                 .build();
         try {
@@ -58,24 +66,36 @@ public class AuthController {
         } catch (IOException | SpotifyWebApiException | org.apache.hc.core5.http.ParseException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        response.sendRedirect("http://localhost:8080/api/top-artists");
+        response.sendRedirect("http://localhost:8080/api/user-top-artists");
         return spotifyApi.getAccessToken();
     }
 
-    @GetMapping(value="top-artists")
-    public Artist[] getUserTopArtists() {
-        final GetUsersTopArtistsRequest getUsersTopArtistsRequest = spotifyApi.getUsersTopArtists()
-                .time_range("medium-term")
+    @GetMapping(value="user-top-artists")
+    public void getUserTopArtists() {
+            final GetUsersTopArtistsRequest getUsersTopArtistsRequest = spotifyApi.getUsersTopArtists()
                 .limit(10)
-                .offset(5)
+                .offset(0)
                 .build();
         try {
             final Paging<Artist> artistPaging = getUsersTopArtistsRequest.execute();
-            return artistPaging.getItems();
+            System.out.println("Primeiro resultado: " + Arrays.stream(artistPaging.getItems()).findFirst());
         } catch (Exception e) {
             System.out.println("We're sorry, something went wrong :(\n" + e.getMessage());
         }
-        return new Artist[0];
+    }
+
+    @GetMapping(value="search/")
+    public void searchArtist(@RequestParam("q") String q) {
+        final SearchArtistsRequest searchArtistsRequest = spotifyApi.searchArtists(q)
+                .limit(10)
+                .offset(0)
+                .build();
+        try {
+            final Paging<Artist> artistPaging = searchArtistsRequest.execute();
+            System.out.println("Primeiro resultado: " + Arrays.stream(artistPaging.getItems()).findFirst());
+        } catch (IOException | SpotifyWebApiException |org.apache.hc.core5.http.ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
 }
--- backend/src/main/resources/static/js/topArtists.js
+++ backend/src/main/resources/static/js/topArtists.js
@@ -1,23 +1,8 @@
 function TopArtists() {
-    const [userTopArtists, setUserTopArtists] = useState();
-
-    useEffect(() => {
         fetch("http://localhost:8080/api/user-top-artists")
             .then(response => response.json())
             .then(data => {
                 console.log(data)
-                setUserTopArtists(data)
             })
-    }, [])
-    return (
-        <div>
-            {userTopArtists ? (
-            userTopArtists.map((artistResult) => {
-                return <h1 key={ artistResult.name}>{artistResult.name}</h1>
-            })
-            ):
-                (<h1>LOADING...</h1>)
-            }
-        </div>
-    );
+            .catch(error=>console.log(error));
 }
